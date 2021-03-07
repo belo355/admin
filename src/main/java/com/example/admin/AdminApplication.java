151a5450262850b3f3c287dcd45dc20f2d79950b
belo355 <edilson.belo@live.com>
2021-03-06 23:08:17 -0300

    init

---
+++ src/main/java/com/example/admin/AdminApplication.java
@@ -0,0 +1,19 @@
+package com.example.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+}
